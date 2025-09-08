401c79854eedce5b35c07cfcc858204c6546e16e
pd-allen <43551964+pd-allen@users.noreply.github.com>
2025-09-08 12:40:22 -0400

    Update 4_🔥_Heatmap.py

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -1,30 +1,9 @@
-import streamlit as st
 import leafmap.foliumap as leafmap
 
-st.set_page_config(layout="wide")
-
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
-
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
-
-st.title("Heatmap")
-
-with st.expander("See source code"):
-    with st.echo():
-        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        m = leafmap.Map(center=[40, -100], zoom=4)
-        m.add_heatmap(
-            filepath,
-            latitude="latitude",
-            longitude="longitude",
-            value="pop_max",
-            name="Heat map",
-            radius=20,
-        )
-m.to_streamlit(height=700)
+m = leafmap.Map()
+cog_url = "HoogeIFFM_cog.tif"
+cog_url2="HoogeWarDiary_cog.tif"
+print(cog_url)
+m.add_raster(cog_url)
+m.add_raster(cog_url2)
+m.to_streamlit(height=900)  # Use this in Streamlit
